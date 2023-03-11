fc9903e9bdf11938c821e8a80d9001fe10f35afd
Abhinay Artham <abhinay@converzai.com>
2023-03-12 01:44:59 +0530

    added trigger listener

---
+++ Models/TriggerListener.cs
@@ -0,0 +1,29 @@
+﻿using Quartz;
+
+namespace WebConsoleApplication.Models
+{
+    public class TriggerListener : ITriggerListener
+    {
+        string ITriggerListener.Name => throw new NotImplementedException();
+
+        Task ITriggerListener.TriggerComplete(ITrigger trigger, IJobExecutionContext context, SchedulerInstruction triggerInstructionCode, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task ITriggerListener.TriggerFired(ITrigger trigger, IJobExecutionContext context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task ITriggerListener.TriggerMisfired(ITrigger trigger, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> ITriggerListener.VetoJobExecution(ITrigger trigger, IJobExecutionContext context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
